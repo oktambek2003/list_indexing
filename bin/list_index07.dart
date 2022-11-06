4fa746097e1b59aad84022271e61fde1de263fb6
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-07 03:41:35 +0500

    7-misol

--- bin/list_index07.dart
+++ bin/list_index07.dart
@@ -6,4 +6,14 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1) {
+  int i = 0;
+  while (i < list1.length) {
+    if (list1[i] == 0) {
+      list1[i] = true;
+    }
+    i += 1;
+  }
+
+  return list1;
+}
